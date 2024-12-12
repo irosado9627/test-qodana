0b91446f63fa469ffe9a973c81984589c568cbd1
irosado9627 <114895884+irosado9627@users.noreply.github.com>
2024-12-12 13:50:21 -0600

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,6 @@ module.exports = {
         '\\.[tj]s$': 'babel-jest',
     },
     coverageDirectory: '.qodana/code-coverage/',
-    coverageReporters: ['lcovonly'],
+    coverageReporters: ['lcovonly', 'json'],
     collectCoverage: true,
 };
